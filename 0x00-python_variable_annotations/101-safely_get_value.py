be9aa252cbeb6840dccf299dafb6df82c30aa9a8
thanks299 <thanksagbeble@gmail.com>
2024-08-02 17:15:07 -0700

    python annotation

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Task 11's module. Safely get a value from a dictionary'''
+
+from typing import TypeVar, Mapping, Any
+
+T = TypeVar('T')  # Type variable for the default value
+
+
+def safely_get_value(dct: Mapping, key: Any, default: T = None) -> Union[Any, T]:
+    '''Safely retrieve a value from a dictionary or return a default value'''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
